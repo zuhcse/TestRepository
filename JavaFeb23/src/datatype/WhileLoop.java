7139628e9aa235023f9ebdff62ed4641b7648646
zuhcse <zuhcse@gmail.com>
2023-02-26 13:04:15 -0500

    space in city name

--- JavaFeb23/src/datatype/WhileLoop.java
+++ JavaFeb23/src/datatype/WhileLoop.java
@@ -4,12 +4,12 @@ public class WhileLoop {
 
 	public static void main(String[] args) {
 			//THis is the name of city
-			String place = "The bronx";
+			String place = " The bronx";
 			int start = 1;
 			
 			while(start<=3)
 			{
-				System.out.println("I am from" + place);
+				System.out.println("I am from" +   place);
 				start ++;
 				
 				
