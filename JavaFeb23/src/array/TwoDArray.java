a4323cd79da1a25e349df0fa12d4bca319bb554e
zuhcse <zuhcse@gmail.com>
2023-03-04 14:09:41 -0500

    2DArray

---
+++ JavaFeb23/src/array/TwoDArray.java
@@ -0,0 +1,32 @@
+package array;
+
+public class TwoDArray {
+
+	public static void main(String[] args) {
+		//2d array is array between array 
+		int [][] my2DArray = new int [2][3];
+		
+		my2DArray[0][0] = 5;
+		my2DArray[0][1] = 6;
+		my2DArray[0][2] = 7;
+		
+		my2DArray[1][0] = 8;
+		my2DArray[1][1] = 8;
+		my2DArray[1][2] = 3;
+		
+		System.out.println(my2DArray[1][1]);
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ JavaFeb23/src/datatype/TypeCasting.java
@@ -0,0 +1,16 @@
+package datatype;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		// type casting is the process of converting
+		// one data type into another data types
+		
+		double myDouble = 56.98;
+		System.out.println("Double value is :" + myDouble);// double value
+		//type casting into int value
+		int myInt = (int)myDouble;
+		System.out.println("Int value is :" + myInt);//56?
+	}
+
+}
